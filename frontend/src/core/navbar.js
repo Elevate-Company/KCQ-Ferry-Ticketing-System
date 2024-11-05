3bea1a69b4d67fe06fb0d01c89a2fc44aaf02461
Jomari <109713343+Noyzki@users.noreply.github.com>
2024-11-05 23:09:17 +0800

    modify navbar and added a bell icon

--- frontend/src/core/navbar.js
+++ frontend/src/core/navbar.js
@@ -8,11 +8,14 @@ function Navbar() {
   return (
     <div className="navbar-card">
       <div className="search-container">
-        <input type="text" placeholder="Search..." className="search-input" />
         <i className="fas fa-search search-icon"></i> {/* Font Awesome search icon */}
+        <input type="text" placeholder="Search..." className="search-input" />
       </div>
-      <div className="profile-avatar">
-        <img src={profileImage} alt="Profile" className="avatar" />
+      <div className="notification-profile">
+        <i className="fas fa-bell notification-icon"></i> {/* Font Awesome bell icon */}
+        <div className="profile-avatar">
+          <img src={profileImage} alt="Profile" className="avatar" />
+        </div>
       </div>
     </div>
   );
