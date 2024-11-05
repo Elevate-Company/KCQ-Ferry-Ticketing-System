bcb7b4e43ae68caa74a5b9f3cca2feb81bc692b6
Jomari <109713343+Noyzki@users.noreply.github.com>
2024-11-06 00:02:57 +0800

    navbar only shows on the dashboard and profile screen

--- frontend/src/core/dashboard.js
+++ frontend/src/core/dashboard.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Navbar from './navbar'; // Import Navbar
 
 function DashboardScreen() {
   return (
     <div>
+      <Navbar /> {/* Navbar included here */}
       <h1>Welcome to the Dashboard!</h1>
-      <p></p>
+      <p>Your content goes here.</p>
     </div>
   );
 }
 
 export default DashboardScreen;
-
-
--- frontend/src/core/profile.js
+++ frontend/src/core/profile.js
@@ -1,9 +1,11 @@
 // frontend/src/core/Profile.js
 import React from 'react';
+import Navbar from './navbar'; 
 
 function Profile() {
   return (
     <div>
+      <Navbar /> {/* Navbar included here */}
       <h1>User Profile</h1>
       <p>This is the Profile page content.</p>
     </div>
