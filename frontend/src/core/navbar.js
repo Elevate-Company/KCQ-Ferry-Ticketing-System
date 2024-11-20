cf7bb2c17ece1b4500d85b34251dd2b62db8c829
Jomari <109713343+Noyzki@users.noreply.github.com>
2024-11-20 18:30:58 +0800

    add an image on the navbar

--- frontend/src/core/navbar.js
+++ frontend/src/core/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/navbar.css';
 import profileImage from '../assets/avatar.png';
+import menuImage from '../assets/menu.png'; // Import the menu image
 
 function Navbar() {
   return (
@@ -10,6 +11,7 @@ function Navbar() {
         <input type="text" placeholder="Search..." className="search-input" />
       </div>
       <div className="notification-profile">
+        <img src={menuImage} alt="Menu" className="menu-icon" /> {/* Display the menu image */}
         <i className="fas fa-bell notification-icon"></i> {}
         <div className="profile-avatar">
           <img src={profileImage} alt="Profile" className="avatar" />
