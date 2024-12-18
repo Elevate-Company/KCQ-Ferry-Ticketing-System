ced8a85391a063053593ab3fb63ca941c56c702a
Jomari Hinayon <hinayonjomari@gmail.com>
2024-12-18 15:37:50 +0800

    Update account view

--- api/views/account.py
+++ api/views/account.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema,  OpenApiParameter, OpenApiResponse
 from rest_framework.decorators import action
 
 from api.serializers import UpdatePasswordSerializer, AccountSerializer
@@ -15,16 +15,25 @@ class AccountViewSet(ViewSet):
     permission_classes = [IsAuthenticated, IsAdmin]
     serializer_class = AccountSerializer
 
+    @extend_schema(
+        responses={200: AccountSerializer},
+        request=AccountSerializer,
+    )
     def list(self, request):
         """Retrieve all account details."""
         accounts = Account.objects.all()
         serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @extend_schema(
+    responses={200: OpenApiResponse(description="Password updated successfully.")},
+    request=UpdatePasswordSerializer
+    )
     @action(detail=False, methods=['post'], url_path='update-password')
     def update_password(self, request):
         """Update the user's password."""
-        serializer = UpdatePasswordSerializer(data=request.data)
+        # Pass the request to the serializer context
+        serializer = UpdatePasswordSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             user = request.user
             user.set_password(serializer.validated_data['new_password'])
@@ -32,6 +41,10 @@ class AccountViewSet(ViewSet):
             return Response({"detail": "Password updated successfully."}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    @extend_schema(
+        responses={200: AccountSerializer},
+        request=None  # No request body for this action
+    )
     @action(detail=False, methods=['get'], url_path='my-account')
     def get_my_account(self, request):
         """Retrieve account details of the authenticated user."""
